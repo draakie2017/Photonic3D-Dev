aebae93bd6ec89ca4fa7629228f8be128f18f093
draakie2017 <info@drakenprint.nl>
2017-04-24 17:48:34 +0100

    Update repoconfig.sh

--- host/bin/repoconfig.sh
+++ host/bin/repoconfig.sh
@@ -10,10 +10,10 @@ if [ -z "$HOME" ] || [ "$HOME" == "/" ]; then
   HOME=~root
 fi
 
-DEFAULT_REPO="Photocentric3D/Photonic3D-Dev"
-DEV_REPO="Photocentric3D/Photonic3D-Dev"
-TESTKIT_REPO="Photocentric3D/Photonic3D-Dev"
-TESTKITDEV_REPO="Photocentric3D/Photonic3D-Dev"
+DEFAULT_REPO="draakie2017/Photonic3D-Dev"
+DEV_REPO="draakie2017/Photonic3D-Dev"
+TESTKIT_REPO="draakie2017/Photonic3D-Dev"
+TESTKITDEV_REPO="draakie2017/Photonic3D-Dev"
 
 CONFIG_PROPS="${HOME}/3dPrinters/config.properties"
 
